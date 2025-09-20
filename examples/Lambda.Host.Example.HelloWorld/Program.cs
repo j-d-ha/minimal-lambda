8098367f555ea3f8ac0231216abeb531e45a8a3c
Jonas Ha <jonas-ha@outlook.com>
2025-09-20 15:26:02 -0400

    **refactor: simplify LambdaApplication example and update dependencies**
    Simplified the HelloWorld example by removing unnecessary service abstractions. Updated package dependencies in `Lambda.Host` and example projects for alignment. Adjusted project references to set `OutputItemType` as `Analyzer` and optimized build properties.

--- examples/Lambda.Host.Example.HelloWorld/Program.cs
+++ examples/Lambda.Host.Example.HelloWorld/Program.cs
@@ -1,30 +1,10 @@
-﻿using System;
-using Lambda.Host;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Lambda.Host;
 using Microsoft.Extensions.Hosting;
 
 var builder = LambdaApplication.CreateBuilder();
-builder.Services.AddSingleton<IService, Service>();
+
 var lambda = builder.Build();
 
-lambda.MapHandler(
-    (Func<IService, string, string>)(
-        (service, input) =>
-        {
-            Console.WriteLine("hello world");
-            return service.GetMessage();
-        }
-    )
-);
+lambda.MapHandler(() => "hello world");
 
 await lambda.RunAsync();
-
-public interface IService
-{
-    string GetMessage();
-}
-
-public class Service : IService
-{
-    public string GetMessage() => "hello world";
-}
