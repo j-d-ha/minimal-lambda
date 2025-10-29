f1fc32848e887e9f3af3d9300b55b02c7d505d33
Jonas Ha <jonas-ha@outlook.com>
2025-10-29 19:04:24 -0400

    feat: add lifecycle example and update HelloWorld example
    - Introduced a new lifecycle example project showcasing shutdown handlers.
    - Updated HelloWorld example:
      - Changed target framework to `.NET 8.0`.
      - Simplified service configuration and improved handler logic.
    - Updated solution file to include the new lifecycle example project.

--- examples/AwsLambda.Host.Example.HelloWorld/Program.cs
+++ examples/AwsLambda.Host.Example.HelloWorld/Program.cs
@@ -1,21 +1,31 @@
-﻿using System;
-using AwsLambda.Host;
+﻿using AwsLambda.Host;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var builder = LambdaApplication.CreateBuilder();
 
-builder.Services.Configure<HostOptions>(options =>
-{
-    options.ShutdownTimeout = TimeSpan.FromSeconds(2);
-});
+builder.Services.AddSingleton<IService, Service>();
 
 var lambda = builder.Build();
 
 lambda.UseClearLambdaOutputFormatting();
 
-lambda.MapHandler(() => new Response("Hello world"));
+lambda.MapHandler(
+    ([Event] Request request, IService service) => new Response(service.GetMessage(request.Name))
+);
 
 await lambda.RunAsync();
 
 internal record Response(string Message);
+
+internal record Request(string Name);
+
+internal interface IService
+{
+    string GetMessage(string name);
+}
+
+internal class Service : IService
+{
+    public string GetMessage(string name) => $"hello {name}";
+}
---
+++ examples/AwsLambda.Host.Example.Lifecycle/Program.cs
@@ -0,0 +1,41 @@
+﻿using AwsLambda.Host;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var builder = LambdaApplication.CreateBuilder();
+
+builder.Services.Configure<HostOptions>(options =>
+{
+    options.ShutdownTimeout = TimeSpan.FromSeconds(2);
+});
+
+var lambda = builder.Build();
+
+lambda.UseClearLambdaOutputFormatting();
+
+lambda.MapHandler(() => new Response("Hello world"));
+
+lambda.OnShutdown(
+    async (services, token) =>
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogInformation("1 Shutting down...");
+        // throw new Exception("test1");
+        await Task.Delay(TimeSpan.FromSeconds(1), token);
+    }
+);
+
+lambda.OnShutdown(
+    async (services, token) =>
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogInformation("2 Shutting down...");
+        // throw new Exception("test2");
+        await Task.Delay(TimeSpan.FromSeconds(1), token);
+    }
+);
+
+await lambda.RunAsync();
+
+internal record Response(string Message);
