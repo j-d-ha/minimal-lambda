079bb3afd50f22b867c49ac88654fbac838b336c
Jonas Ha <jonas-ha@outlook.com>
2025-11-03 19:17:39 -0500

    feat(hosting): add `OnInit` handler and support for initialization timeout configuration
    Introduced an `OnInit` handler to the lifecycle, allowing custom logic to execute during initialization. Added `InitTimeout` configuration for managing initialization duration. Updated example to demonstrate usage.

--- examples/AwsLambda.Host.Example.Lifecycle/Program.cs
+++ examples/AwsLambda.Host.Example.Lifecycle/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using AwsLambda.Host;
 using Microsoft.Extensions.DependencyInjection;
@@ -8,8 +9,9 @@ var builder = LambdaApplication.CreateBuilder();
 
 builder.Services.ConfigureLambdaHost(options =>
 {
-    options.RuntimeShutdownDuration = TimeSpan.FromSeconds(3);
-    options.RuntimeShutdownDurationBuffer = TimeSpan.FromSeconds(1);
+    options.InitTimeout = TimeSpan.FromSeconds(5);
+    options.ShutdownDuration = TimeSpan.FromSeconds(3);
+    options.ShutdownDurationBuffer = TimeSpan.FromSeconds(1);
 });
 
 builder.Services.AddSingleton<IService, Service>();
@@ -23,6 +25,29 @@ lambda.OnInit(
     {
         Console.WriteLine("Initializing...");
         return Task.FromResult(true);
+    }
+);
+
+lambda.OnInit(
+    async Task<bool> (services, cancellationToken) =>
+    {
+        var stopwatch = Stopwatch.StartNew();
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            Console.WriteLine(
+                $"Waiting for init to timeout. {stopwatch.ElapsedMilliseconds}ms elapsed"
+            );
+            try
+            {
+                await Task.Delay(TimeSpan.FromMilliseconds(100), cancellationToken);
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+
+        return true;
     }
 );
 
