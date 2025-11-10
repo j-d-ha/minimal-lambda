b098200788bb4a7c34029192a1b3f8703d45ebf3
Jonas Ha <jonas-ha@outlook.com>
2025-11-10 18:21:21 -0500

    feat(examples): add new example project for Lambda API Gateway events
    - Introduced `AwsLambda.Host.Example.Events` project to demonstrate event handling
    - Configured project for `net8.0` with support for Lambda execution and testing
    - Set up `launchSettings.json` with environment variables for local debugging
    - Included `appsettings.json` with logging configuration and Lambda-specific settings

---
+++ examples/AwsLambda.Host.Example.Events/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using AwsLambda.Host;
+using AwsLambda.Host.APIGatewayEvents;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = LambdaApplication.CreateBuilder();
+
+builder.Services.ConfigureLambdaHostOptions(options =>
+{
+    options.ClearLambdaOutputFormatting = true;
+});
+
+builder.Services.Configure<JsonSerializerOptions>(options =>
+{
+    options.PropertyNameCaseInsensitive = true;
+    options.PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower;
+});
+
+var lambda = builder.Build();
+
+lambda.MapHandler(
+    ([Event] APIGatewayProxyRequest<Request> request) =>
+        new APIGatewayProxyResponse<Response>
+        {
+            Body = new Response($"Hello {request.Body!.Name}!", DateTime.UtcNow),
+            StatusCode = 200,
+            Headers = new Dictionary<string, string> { ["Content-Type"] = "application/json" },
+        }
+);
+
+await lambda.RunAsync();
+
+internal record Response(string Message, DateTime TimestampUtc);
+
+internal record Request(string Name);
