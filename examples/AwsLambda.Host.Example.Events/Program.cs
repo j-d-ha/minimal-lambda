1ccf575690911071580cb512e4a6a2834bf3c9ee
Jonas Ha <jonas-ha@outlook.com>
2025-11-10 19:57:15 -0500

    feat(examples): add example handling SQS events in Lambda project
    - Updated `AwsLambda.Host.Example.Events` to include `AwsLambda.Host.SQSEvent` project reference
    - Demonstrated SQS event processing with deserialization and batch response handling
    - Showcased logging and failure case simulation in the new handler examples in `Program.cs`

--- examples/AwsLambda.Host.Example.Events/Program.cs
+++ examples/AwsLambda.Host.Example.Events/Program.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Text.Json;
 using AwsLambda.Host;
 using AwsLambda.Host.APIGatewayEvents;
+using AwsLambda.Host.SQSEvent;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 var builder = LambdaApplication.CreateBuilder();
 
@@ -29,6 +31,31 @@ lambda.MapHandler(
             StatusCode = 200,
             Headers = new Dictionary<string, string> { ["Content-Type"] = "application/json" },
         }
+);
+
+// this wont compile as we can only have a single handler per lambda function
+lambda.MapHandler(
+    ([Event] SQSEvent<Request> sqsEvent, ILogger logger) =>
+    {
+        var responses = new SQSBatchResponse();
+
+        foreach (var record in sqsEvent.Records)
+        {
+            // simulate failure if we get bad data
+            if (record.Body!.Name == "John")
+                responses.BatchItemFailures.Add(
+                    new Amazon.Lambda.SQSEvents.SQSBatchResponse.BatchItemFailure
+                    {
+                        ItemIdentifier = record.MessageId,
+                    }
+                );
+
+            // otherwise, log the message
+            logger.LogInformation("Hello {name}!", record.Body.Name);
+        }
+
+        return responses;
+    }
 );
 
 await lambda.RunAsync();
