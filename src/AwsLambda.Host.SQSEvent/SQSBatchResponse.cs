a315a19c91e31753912becc5d3eb95f6310a01e2
Jonas Ha <jonas-ha@outlook.com>
2025-11-11 18:44:33 -0500

    feat(aws-lambda): remove SQSBatchResponse and SQSEvent for simpler event handling
    - Deleted `SQSBatchResponse` and `SQSEvent` classes to streamline the library and reduce duplication
    - Rely directly on `Amazon.Lambda.SQSEvents` for SQS event and batch response handling

--- src/AwsLambda.Host.SQSEvent/SQSBatchResponse.cs
+++
@@ -1,7 +0,0 @@
-using System.Runtime.Serialization;
-
-namespace AwsLambda.Host.SQSEvent;
-
-/// <inheritdoc />
-[DataContract]
-public class SQSBatchResponse : Amazon.Lambda.SQSEvents.SQSBatchResponse;
