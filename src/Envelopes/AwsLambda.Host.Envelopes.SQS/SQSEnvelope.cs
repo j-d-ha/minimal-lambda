797284baf1c266eb1ccf14dc5dd72f90dd1fbd1e
Jonas Ha <jonas-ha@outlook.com>
2025-11-14 07:27:44 -0500

    refactor(envelopes): update `SQSEnvelope` to replace `IJsonSerializable` with `IEnvelope`
    - Replaced `IJsonSerializable` with `IEnvelope` in `SQSEnvelope` to align with recent abstractions.
    - Added `ExtractPayload` and `PackPayload` methods for payload serialization/deserialization.
    - Removed `RegisterConverter` logic in favor of improved extensibility with `JsonSerializerOptions`.

--- src/Envelopes/AwsLambda.Host.Envelopes.SQS/SQSEnvelope.cs
+++ src/Envelopes/AwsLambda.Host.Envelopes.SQS/SQSEnvelope.cs
@@ -1,17 +1,26 @@
+using System.Text.Json;
 using System.Text.Json.Serialization;
 using Amazon.Lambda.SQSEvents;
 
 namespace AwsLambda.Host.Envelopes.SQS;
 
 /// <inheritdoc cref="SQSEvent" />
-public class SQSEnvelope<T> : SQSEvent, IJsonSerializable
+public class SQSEnvelope<T> : SQSEvent, IEnvelope
 {
     /// <summary>Get and sets the Records</summary>
     public new required List<SQSMessageEnvelope> Records { get; set; }
 
-    /// <inheritdoc />
-    public static void RegisterConverter(IList<JsonConverter> converters) =>
-        converters.Add(new SQSEnvelopeJsonConverter<T>());
+    public void ExtractPayload(JsonSerializerOptions options)
+    {
+        foreach (var record in Records)
+            record.Body = JsonSerializer.Deserialize<T>(((SQSMessage)record).Body, options);
+    }
+
+    public void PackPayload(JsonSerializerOptions options)
+    {
+        foreach (var record in Records)
+            ((SQSMessage)record).Body = JsonSerializer.Serialize(record.Body, options);
+    }
 
     /// <inheritdoc />
     public class SQSMessageEnvelope : SQSMessage
