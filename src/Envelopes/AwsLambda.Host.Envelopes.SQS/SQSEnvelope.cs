cbec81e818cb09a80d160d2d1378b40914add94e
Jonas Ha <jonas-ha@outlook.com>
2025-11-14 21:21:02 -0500

    refactor(envelopes): remove `SQSEnvelopeJsonConverter` and update `SQSEnvelope` payload handling
    - Deleted `SQSEnvelopeJsonConverter` to simplify the codebase and reduce redundancy.
    - Updated `SQSEnvelope` to use `EnvelopeOptions` instead of `JsonSerializerOptions`.
    - Renamed `Body` to `BodyContent` in `SQSMessageEnvelope` for improved clarity.
    - Adjusted serialization/deserialization logic to operate directly within `SQSEnvelope`.

--- src/Envelopes/AwsLambda.Host.Envelopes.SQS/SQSEnvelope.cs
+++ src/Envelopes/AwsLambda.Host.Envelopes.SQS/SQSEnvelope.cs
@@ -1,6 +1,7 @@
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using Amazon.Lambda.SQSEvents;
+using AwsLambda.Host.Options;
 
 namespace AwsLambda.Host.Envelopes.SQS;
 
@@ -10,23 +11,23 @@ public class SQSEnvelope<T> : SQSEvent, IEnvelope
     /// <summary>Get and sets the Records</summary>
     public new required List<SQSMessageEnvelope> Records { get; set; }
 
-    public void ExtractPayload(JsonSerializerOptions options)
+    public void ExtractPayload(EnvelopeOptions options)
     {
         foreach (var record in Records)
-            record.Body = JsonSerializer.Deserialize<T>(((SQSMessage)record).Body, options);
+            record.BodyContent = JsonSerializer.Deserialize<T>(record.Body, options.JsonOptions);
     }
 
-    public void PackPayload(JsonSerializerOptions options)
+    public void PackPayload(EnvelopeOptions options)
     {
         foreach (var record in Records)
-            ((SQSMessage)record).Body = JsonSerializer.Serialize(record.Body, options);
+            record.Body = JsonSerializer.Serialize(record.BodyContent, options.JsonOptions);
     }
 
     /// <inheritdoc />
     public class SQSMessageEnvelope : SQSMessage
     {
-        /// <summary>Get and sets the Body</summary>
+        /// <summary>Get and sets the BodyContent</summary>
         [JsonIgnore]
-        public new required T? Body { get; set; }
+        public required T? BodyContent { get; set; }
     }
 }
