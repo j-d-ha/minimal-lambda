93cd02d790a64ba366c178f9b295f9ff67c66b44
Jonas Ha <jonas-ha@outlook.com>
2025-11-22 18:04:25 -0500

    refactor(hello-world-example): simplify handler and remove unused imports
    - Replaced `Stream`-based handler with direct `Request` and `Response` objects for clarity.
    - Removed unused imports: `System.IO` and `System.Text.Json` for cleaner and more maintainable code.
    - Updated middleware logging messages for consistent format.

--- examples/AwsLambda.Host.Example.HelloWorld/Program.cs
+++ examples/AwsLambda.Host.Example.HelloWorld/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.IO;
-using System.Text.Json;
 using System.Text.Json.Serialization;
 using AwsLambda.Host.Builder;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,24 +13,14 @@ var lambda = builder.Build();
 lambda.UseMiddleware(
     async (context, next) =>
     {
-        Console.WriteLine("[Middleware 1] Before");
+        Console.WriteLine("[Middleware 1]: Before");
         await next(context);
-        Console.WriteLine("[Middleware 1] After");
+        Console.WriteLine("[Middleware 1]: After");
     }
 );
 
 lambda.MapHandler(
-    Stream ([Event] Stream request, IService service) =>
-    {
-        var @event = JsonSerializer.Deserialize<Request>(request);
-
-        var response = new Response(service.GetMessage(@event?.Name ?? "EMPTY"));
-
-        var outputStream = new MemoryStream();
-        JsonSerializer.Serialize(outputStream, response);
-        outputStream.Position = 0;
-        return outputStream;
-    }
+    ([Event] Request request, IService service) => new Response(service.GetMessage(request.Name))
 );
 
 await lambda.RunAsync();
