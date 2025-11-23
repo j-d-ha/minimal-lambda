eff69d83680fbd7b3909648e68d378ef431a7e2f
Jonas Ha <jonas-ha@outlook.com>
2025-11-23 16:13:38 -0500

    refactor(benchmarks): update builder benchmarks and `.gitignore`
    - Removed unused `CreateEmptyBuilder` and `CreateSlimBuilder` benchmarks.
    - Updated `CreateBuilder` benchmark to exclude the `Build()` method.
    - Added `CreateBuilderDisableDefaults` benchmark with `DisableDefaults` option.
    - Updated `.gitignore` to exclude `BenchmarkDotNet.Artifacts/` directory.

--- benchmarks/AwsLambda.Host.Benchmarks/Benchmarks.cs
+++ benchmarks/AwsLambda.Host.Benchmarks/Benchmarks.cs
@@ -1,29 +1,14 @@
 ﻿using AwsLambda.Host.Builder;
 using BenchmarkDotNet.Attributes;
-using Microsoft.Extensions.Hosting;
 
 namespace AwsLambda.Host.Benchmarks;
 
 public class CreateBuilderBenchmarks
 {
     [Benchmark]
-    public void CreateBuilder()
-    {
-        var builder = LambdaApplication.CreateBuilder();
-        builder.Build();
-    }
+    public void CreateBuilder() => LambdaApplication.CreateBuilder();
 
     [Benchmark]
-    public void CreateEmptyBuilder()
-    {
-        var builder = LambdaApplication.CreateEmptyBuilder(new HostApplicationBuilderSettings());
-        builder.Build();
-    }
-
-    [Benchmark]
-    public void CreateSlimBuilder()
-    {
-        var builder = LambdaApplication.CreateSlimBuilder();
-        builder.Build();
-    }
+    public void CreateBuilderDisableDefaults() =>
+        LambdaApplication.CreateBuilder(new LambdaApplicationOptions { DisableDefaults = true });
 }
