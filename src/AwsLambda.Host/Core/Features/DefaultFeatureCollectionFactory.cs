823311eb48550757598be7bb86148a9d6c44d50d
Jonas Ha <jonas-ha@outlook.com>
2025-11-29 13:27:28 -0500

    refactor(core): enhance null safety in DefaultFeatureCollectionFactory
    - Added null-checking logic to filter out null `IFeatureProvider` instances.
    - Simplified constructor and improved internal field initialization.

--- src/AwsLambda.Host/Core/Features/DefaultFeatureCollectionFactory.cs
+++ src/AwsLambda.Host/Core/Features/DefaultFeatureCollectionFactory.cs
@@ -1,7 +1,11 @@
 namespace AwsLambda.Host.Core;
 
-internal class DefaultFeatureCollectionFactory(IEnumerable<IFeatureProvider> featureProviders)
-    : IFeatureCollectionFactory
+internal class DefaultFeatureCollectionFactory : IFeatureCollectionFactory
 {
-    public IFeatureCollection Create() => new DefaultFeatureCollection(featureProviders);
+    private readonly IEnumerable<IFeatureProvider> _featureProviders;
+
+    public DefaultFeatureCollectionFactory(IEnumerable<IFeatureProvider?> featureProviders) =>
+        _featureProviders = featureProviders.Where(x => x is not null).Select(x => x!).ToArray();
+
+    public IFeatureCollection Create() => new DefaultFeatureCollection(_featureProviders);
 }
