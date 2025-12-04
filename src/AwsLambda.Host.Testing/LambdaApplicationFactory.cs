2583f1bfb1ba3b8adb7bdcd56ca45ee124f58051
Jonas Ha <jonas-ha@outlook.com>
2025-12-04 16:41:04 -0500

    feat(testing): add LambdaApplicationFactory to AwsLambda.Host.Testing
    - Introduced `LambdaApplicationFactory` for managing and testing Lambda application lifecycle.
    - Implemented proper disposal handling with `Dispose` and `DisposeAsync` methods.
    - Ensured compatibility with `IHost` for seamless integration and teardown.

---
+++ src/AwsLambda.Host.Testing/LambdaApplicationFactory.cs
@@ -0,0 +1,50 @@
+using Microsoft.Extensions.Hosting;
+
+namespace AwsLambda.Host.Testing;
+
+public class LambdaApplicationFactory<TStartup> : IDisposable, IAsyncDisposable
+    where TStartup : class
+{
+    private bool _disposed;
+    private bool _disposedAsync;
+
+    private IHost? _host;
+
+    public async ValueTask DisposeAsync()
+    {
+        if (_disposed)
+            return;
+
+        if (_disposedAsync)
+            return;
+
+        if (_host != null)
+        {
+            await _host.StopAsync().ConfigureAwait(false);
+            _host?.Dispose();
+        }
+
+        _disposedAsync = true;
+
+        Dispose(true);
+
+        GC.SuppressFinalize(this);
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    private void Dispose(bool disposing)
+    {
+        if (_disposed)
+            return;
+
+        if (disposing && !_disposedAsync)
+            DisposeAsync().AsTask().ConfigureAwait(false).GetAwaiter().GetResult();
+
+        _disposed = true;
+    }
+}
