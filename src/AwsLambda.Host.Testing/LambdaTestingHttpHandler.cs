5a0756e544a30b95e5640d63a1ea6b36b7906775
Jonas Ha <jonas-ha@outlook.com>
2025-12-05 16:32:38 -0500

    feat(testing): enhance LambdaTestingHttpHandler with request/response channel support
    - Added constructor to `LambdaTestingHttpHandler` accepting request and response channels.
    - Implemented `SendAsync` to pass requests to `requestChannel` and receive responses from `responseChannel`.

--- src/AwsLambda.Host.Testing/LambdaTestingHttpHandler.cs
+++ src/AwsLambda.Host.Testing/LambdaTestingHttpHandler.cs
@@ -1,3 +1,21 @@
+using System.Threading.Channels;
+
 namespace AwsLambda.Host.Testing;
 
-public class LambdaTestingHttpHandler : DelegatingHandler { }
+public class LambdaTestingHttpHandler(
+    Channel<HttpRequestMessage> requestChanel,
+    Channel<HttpResponseMessage> responseChanel
+) : DelegatingHandler
+{
+    protected override async Task<HttpResponseMessage> SendAsync(
+        HttpRequestMessage request,
+        CancellationToken cancellationToken
+    )
+    {
+        // pass the request out to the client
+        await requestChanel.Writer.WriteAsync(request, cancellationToken);
+
+        // block here until the client sends a response. There may not be a response.
+        return await responseChanel.Reader.ReadAsync(cancellationToken);
+    }
+}
